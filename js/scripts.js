178eefea660773544fc15fec211716020a6bdc0d
sebastianekstrom <seb.ekstrom@gmail.com>
2019-09-12 11:35:13 +0200

    Remove jQuery/Bootstrap dependencies, add favicon

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
-$('.video-player').click(function() {
-    var player = $(this),
-        url = player.attr('data-url'),
-        img = player.children('img');
-    if (url && !player.hasClass('playing')) {
-        $('<iframe frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>')
-            .attr('src', url)
-            .attr('width', img.width())
-            .attr('height', img.height())
-            .appendTo(player);
-        player.addClass('playing');
-    }
+var player = document.getElementById("videoPlayer");
+
+player.addEventListener("click", function() {
+  var videoURL = player.getAttribute("data-url");
+  var image = player.querySelector("img");
+  var playingClass = "playing";
+  var imageHeight = image.height;
+  var imageWidth = image.width;
+
+  if (!videoURL && player.classList.contains(playingClass)) return;
+
+  var iFrame = document.createElement("iframe");
+  iFrame.setAttribute("src", videoURL);
+  iFrame.setAttribute("frameborder", "0");
+  iFrame.setAttribute("allowFullScreen", "true");
+  iFrame.setAttribute("mozallowfullscreen", "true");
+  iFrame.setAttribute("webkitAllowFullScreen", "true");
+  iFrame.setAttribute("width", imageWidth);
+  iFrame.setAttribute("height", imageHeight);
+  player.appendChild(iFrame);
+  player.classList.add(playingClass);
 });
